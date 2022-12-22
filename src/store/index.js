4a54d7bd1301542b4c94be0d8e3f9fb3314efbfb
vipinrwt95 <vipinrwt9@gmail.com>
2022-12-22 13:29:02 +0530

    reducr

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import {createStore} from 'redux'
+
+
+const counterReducer=(state={counter:0},action)=>
+{
+ if(action.type=='increment')
+ {
+    return{
+        counter:state.counter+2,
+    }
+ }
+ if(action.type==='decrement')
+ {
+    return {
+        counter:state.counter-2
+    }
+ }
+   return state;
+}
+const store=createStore(counterReducer);
+
+
+export default store;
+
