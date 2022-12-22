0cc56cb9e04f30125ff0dbe9a1a0ae20a39794eb
vipinrwt95 <vipinrwt9@gmail.com>
2022-12-22 13:28:38 +0530

    reducer functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux'
+import {Provider} from 'react-redux';
+import store from './store/index';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider><App /></Provider>);
+root.render(<Provider store={store}><App /></Provider>);
